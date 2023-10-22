e0ffca14e9fe1b08f188195800bd05cc6b70e1a3
gastyft <gastyftacla1@hotmail.com>
2023-10-22 05:54:40 -0300

    Creacion de librerias

---
+++  TP-Final-Progra2/menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "empleado_de_laboratorio.h"
+#include "pacientes.h"
+#include "practicas_x_ingreso.h"
+#include "practica_de_laboratorio.h"
+#include "ingreso_de_laboratorio.h"
+
+
+///PROTOTIPADO
+
+
+
+#endif // MENU_H_INCLUDED
